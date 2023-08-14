7fadf179da98f72a56b1fce23b8f14e63b0b5313
XIAOJIANG LIN <1227891082@qq.com>
2023-08-14 20:03:07 +0800

    Final second Version

--- router/router.go
+++ router/router.go
@@ -11,16 +11,13 @@ func InitRouter(r *gin.Engine) {
 
 	apiRouter := r.Group("/ct")
 	//根据id获取页面信息 /ct/page/1
+	//获取页面信息
 	apiRouter.GET("/page/:id", func(context *gin.Context) {
 		topicId := context.Param("id")
 		data := controller.QueryPageInfo(topicId)
 		context.JSON(http.StatusOK, data)
 	})
-	apiRouter.GET("/hello", func(context *gin.Context) {
-		context.HTML(http.StatusOK, "index.html", gin.H{
-			"msg": "hello",
-		})
-	})
+	//添加帖子
 	apiRouter.POST("post/do", func(context *gin.Context) {
 		uid, _ := context.GetPostForm("uid")
 		topicId, _ := context.GetPostForm("topic_id")
